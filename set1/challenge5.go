7c133cf20f4d7c5a352f35632afa1e02f6d287c7
i.kushmantsev <i.kushmantsev@fun-box.ru>
2018-09-22 21:33:33 +0400

    ch5

---
+++ set1/challenge5.go
@@ -0,0 +1,14 @@
+package cryptopals
+
+func RepeatingKeyXorEncrypt(input []byte, key []byte) []byte {
+	if len(key) == 0 {
+		panic("Key length is 0")
+	}
+	res := make([]byte, len(input))
+	var j int
+	for i, b := range input {
+		j = i % len(key)
+		res[i] = b ^ key[j]
+	}
+	return res
+}
---
+++ set1/challenge5_test.go
@@ -0,0 +1,14 @@
+package cryptopals
+
+import "testing"
+import "encoding/hex"
+
+func TestRepeatingKeyXorEncrypt(t *testing.T) {
+	input := "Burning 'em, if you ain't quick and nimble\nI go crazy when I hear a cymbal"
+	key := "ICE"
+	res := RepeatingKeyXorEncrypt([]byte(input), []byte(key))
+	want := "0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f"
+	if want != hex.EncodeToString(res) {
+		t.Errorf("Wrong result: %s", hex.EncodeToString(res))
+	}
+}
