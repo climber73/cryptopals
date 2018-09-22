e4f167a3a7b71f10f2695b939a972d7119d6f1f5
i.kushmantsev <i.kushmantsev@fun-box.ru>
2018-09-22 10:35:23 +0400

    refactor ch3: use autogenerated english language corpus

--- set1/challenge3.go
+++ set1/challenge3.go
@@ -2,63 +2,37 @@ package cryptopals
 
 // http://cryptopals.com/sets/1/challenges/3
 
-import "fmt"
-import "encoding/hex"
-
-var m = make(map[byte]byte)
-var scores = make(map[byte]int)
-
-func FindXorCipher(input string) byte {
-	fmt.Printf("Input: %s\n", input)
-	bytes, err := hex.DecodeString(input)
-	if err != nil {
-		panic("Can't decode input string")
-	}
-	tmp := make([]byte, len(bytes))
-	for key := byte(0x00); key < 0xff; key++ {
-		copy(tmp, bytes)
-		initMap()
-		decipherBytes(tmp, key)
-		fmt.Printf("Key: %c ", key)
-		scores[key] = evaluateScore(tmp)
-	}
-
-	max := 0
+func DecodeXoredBytes(input []byte, c map[byte]float32) ([]byte, byte) {
+	decoded := make([]byte, len(input))
+	tmp := make([]byte, len(input))
 	var key byte
-	for k, s := range scores {
-		if s > max {
-			max = s
-			key = k
+	var max_score float32
+	for k := 0x00; k <= 0xff; k++ {
+		decodeBytes(input, tmp, byte(k))
+		score := evaluateScore(tmp, c)
+		if score > max_score {
+			key = byte(k)
+			copy(decoded, tmp)
+			max_score = score
 		}
 	}
-	return key
+
+	return decoded, key
 }
 
-func initMap() {
-	for c := 'a'; c < 'z'; c++ {
-		m[byte(c)] = 0
+func evaluateScore(bytes []byte, c map[byte]float32) float32 {
+	var score float32
+	for _, b := range bytes {
+		score += c[b]
 	}
+	return score
 }
 
-func evaluateScore(bytes []byte) int {
-	fmt.Printf("Eval: \"%s\"... ", bytes)
-	for c := byte('a'); c < byte('z'); c++ {
-		for i := 0; i < len(bytes); i++ {
-			if c == bytes[i] {
-				m[c]++
-			}
-		}
+func decodeBytes(input []byte, output []byte, k byte) {
+	if len(input) != len(output) {
+		panic("different lengths")
 	}
-	sum := 0
-	for c := range m {
-		sum += int(m[c])
-	}
-	fmt.Printf("sum=%d\n", sum)
-	return sum
-}
-
-func decipherBytes(bytes []byte, c byte) {
-	for i := 0; i < len(bytes); i++ {
-		bytes[i] = bytes[i] ^ c
+	for i, value := range input {
+		output[i] = value ^ k
 	}
 }
--- set1/challenge3_test.go
+++ set1/challenge3_test.go
@@ -2,9 +2,38 @@ package cryptopals
 
 import "testing"
 import "fmt"
+import "io/ioutil"
+import "encoding/hex"
+
+const ENGLISH_TEXT_FILE = "test_data/peterpan.txt"
 
 func TestFindXorCipher(t *testing.T) {
-	input := "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736"
-	key := FindXorCipher(input)
-	fmt.Printf("The key is '%c' (%d)\n", key, key)
+	text, err := ioutil.ReadFile(ENGLISH_TEXT_FILE)
+	if err != nil {
+		t.Errorf("can't read file: %s", ENGLISH_TEXT_FILE)
+	}
+	corpus := buildCorpus(text)
+	input := decodeHex("1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736", t)
+	res, key := DecodeXoredBytes(input, corpus)
+	fmt.Printf("%s (key: %c)\n", res, key)
+}
+
+func buildCorpus(text []byte) map[byte]float32 {
+	corpus := make(map[byte]float32)
+	for _, b := range text {
+		corpus[b]++
+	}
+	length := float32(len(text))
+	for k, v := range corpus {
+		corpus[k] = v / length
+	}
+	return corpus
+}
+
+func decodeHex(s string, t *testing.T) []byte {
+	res, err := hex.DecodeString(s)
+	if err != nil {
+		t.Error(err)
+	}
+	return res
 }
