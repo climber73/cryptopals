cc5eda11c9b28f9ae251e0b08bdc785340c05f86
i.kushmantsev <i.kushmantsev@fun-box.ru>
2019-02-17 23:05:41 +0400

    add decrypted text to tests

--- set2/ch10_test.go
+++ set2/ch10_test.go
@@ -3,6 +3,7 @@ package set2
 import "testing"
 import "io/ioutil"
 import "encoding/base64"
+import "bytes"
 
 func TestCBCDecrypt(t *testing.T) {
 	path := "test_data/10.txt"
@@ -15,5 +16,13 @@ func TestCBCDecrypt(t *testing.T) {
 		t.Errorf("Can't decode base64 to bytes: %s", err)
 	}
 	res := CBCDecrypt(siphertext, []byte("YELLOW SUBMARINE"), make([]byte, 16))
-	t.Logf("Decrypted text:\n%s\n", res)
+
+	decryptedPath := "test_data/10-decrypted.txt"
+	decryptedText, err := ioutil.ReadFile(decryptedPath)
+	if err != nil {
+		t.Errorf("Can't read file: %s", decryptedPath)
+	}
+	if bytes.Compare(decryptedText, res) != 0 {
+		t.Errorf("expected:\n<%x>\ngot:\n<%x>", decryptedText, res)
+	}
 }
